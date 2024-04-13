56d2999a107f09a3a34bced8b676e724a90f0290
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2024-04-13 12:28:07 +0100

    ci(semantic-release): custom formatting

--- release.config.js
+++ release.config.js
@@ -5,38 +5,19 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        [
-            "@semantic-release/changelog",
+        ["@semantic-release/changelog", {"changelogFile": "CHANGELOG.md"}],
+        ["@semantic-release/git",
             {
-                "changelogFile": "CHANGELOG.md"
-            }
-        ],
-        [
-            "@semantic-release/git",
-            {
-                "assets": [
-                    "CHANGELOG.md"
-                ],
+                "assets": ["CHANGELOG.md"],
                 "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-            }
-        ],
-        "semantic-release-docker-buildx",
-        [
-            "@semantic-release/github",
-            {
-                "addReleases": "bottom"
-            }
-        ]
+            }],
+        "semantic-release-docker-buildx", ["@semantic-release/github", {"addReleases": "bottom"}]
     ],
     publish: [
         {
             "path": "semantic-release-docker-buildx",
-            "buildArgs": {
-                "COMMIT_TAG": "$GIT_SHA"
-            },
-            "imageNames": [
-                "betterdev/k8s-ci-cd"
-            ],
+            "buildArgs": {"COMMIT_TAG": "$GIT_SHA"},
+            "imageNames": ["betterdev/k8s-ci-cd"],
             "platforms": [
                 "linux/amd64",
                 "linux/arm64",
