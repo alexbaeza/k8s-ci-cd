ff6a325d8fb16061251017d841715fab3a481bcf
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2024-04-13 12:30:39 +0100

    ci(semantic-release): Migrate semantic release (#8)
    * ci(semantic-release): Migrate semantic release to custom github action to avoid having to reduce repo files and complexity

---
+++ release.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    branches: [
+        {name: 'main'},
+    ],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/changelog", {"changelogFile": "CHANGELOG.md"}],
+        ["@semantic-release/git",
+            {
+                "assets": ["CHANGELOG.md"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }],
+        "semantic-release-docker-buildx",
+        ["@semantic-release/github", {"addReleases": "bottom"}]
+    ],
+    publish: [
+        {
+            "path": "semantic-release-docker-buildx",
+            "buildArgs": {"COMMIT_TAG": "$GIT_SHA"},
+            "imageNames": ["betterdev/k8s-ci-cd"],
+            "platforms": [
+                "linux/amd64",
+                "linux/arm64",
+                "linux/arm/v7"
+            ]
+        },
+        "@semantic-release/github"
+    ]
+};
