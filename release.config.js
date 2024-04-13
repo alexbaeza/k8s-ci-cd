158a677d4d1aa37a62fe7630abc0624e5798c71c
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2024-04-13 12:24:56 +0100

    ci(semantic-release): remove packaege.json

--- release.config.js
+++ release.config.js
@@ -15,7 +15,6 @@ module.exports = {
             "@semantic-release/git",
             {
                 "assets": [
-                    "package.json",
                     "CHANGELOG.md"
                 ],
                 "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
